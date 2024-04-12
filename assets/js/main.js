1b2bd4e643de9cad7ae009fc336abfb23a6dbc4a
Nguyen The Hoai Nam <76147564+nthnam17@users.noreply.github.com>
2024-04-13 02:19:47 +0700

    @@

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,242 +1,19 @@
-// (function($) {
 
-//     $('body').on('mouseenter mouseleave', '.nav-item', function(e) {
-//         if ($(window).width() > 750) {
-//             var _d = $(e.target).closest('.nav-item');
-//             _d.addClass('show');
-//             setTimeout(function() {
-//                 _d[_d.is(':hover') ? 'addClass' : 'removeClass']('show');
-//             }, 1);
-//         }
-//     });
-
-//     // js count number
-//     var isAlreadyRun = false;
-//     $(window).scroll(function() {
-
-//         $('.counter-show').each(function(i) {
-//             var bottom_of_object = $(this).position().top + $(this).outerHeight() / 2;
-//             var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-//             if (bottom_of_window > (bottom_of_object + 20)) {
-//                 if (!isAlreadyRun) {
-//                     $('.count-number').each(function() {
-
-//                         $(this).prop('Counter', 0).animate({
-//                             Counter: $(this).text()
-//                         }, {
-//                             duration: 3000,
-//                             easing: 'swing',
-//                             step: function(now) {
-//                                 $(this).text(Math.ceil(now));
-//                             }
-//                         });
-//                     });
-//                 }
-//                 isAlreadyRun = true;
-//             }
-//         });
-//     });
-
-// // js back to top
-//     const scrollBtn = document.querySelector('.scroll-btn');
-//     window.addEventListener('scroll', () => {
-//         if (document.body.scrollTop > 20 || document
-//             .documentElement
-//             .scrollTop > 20) {
-//             scrollBtn.style.display = 'block';
-
-//         } else {
-//             scrollBtn.style.display = 'none';
-//         }
-//     })
-//     scrollBtn.addEventListener('click', () => {
-//         window.scroll({
-//             top: 0,
-//             behavior: "smooth"
-//         })
-//     })
-
-
-
-// })(jQuery);
-
-
-// // js active background-color menu languge
-// $( '#languge .btnLanguge ' ).on( 'click', function () {
-//     // alert('123');
-//     $( '#languge ' ).find( 'a.active' ).removeClass( 'active' );
-//     $( this ).parent( 'a' ).addClass( 'active' );
-// });
-
-
-// function validateEmail(email) {
-//     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//     return re.test(String(email).toLowerCase());
-// }
-// function validatePhone(email) {
-//     const re = /^([0-9]{10})|(\([0-9]{3}\)\s+[0-9]{3}\-[0-9]{4})$/;
-//     return re.test(String(email).toLowerCase());
-// }
-
-// function showAlert(message, type, closeDelay) {
-
-//     var $cont = $("#alerts-container");
-
-//     if ($cont.length == 0) {
-//         // alerts-container does not exist, create it
-//         $cont = $('<div id="alerts-container">')
-//             .css({
-//                 position: "fixed"
-//                 ,width: "20%"
-//                 ,right: "10px"
-//                 ,top: "10px"
-//                 ,fontSize: "14px"
-//                 ,"z-index": "99999"
-//             })
-//             .appendTo($("body"));
-//     }
-
-//     // default to alert-info; other options include success, warning, danger
-//     type = type || "info";
-
-//     // create the alert div
-//     var alert = $('<div>')
-//         .addClass("fade in show alert alert-" + type)
-//         .append(
-//             $('<button type="button" class="close" data-dismiss="alert">')
-//                 .append("&times;")
-//         )
-//         .append('<i class="mdi mdi-alert" aria-hidden="true"></i>')
-//         .append(message);
-
-//     // add the alert div to top of alerts-container, use append() to add to bottom
-//     $cont.prepend(alert);
-
-//     // if closeDelay was passed - set a timeout to close the alert
-//     if (closeDelay)
-//         window.setTimeout(function() { alert.remove() }, closeDelay);
-// }
-
-// var isMobile = {
-//     Android: function() {
-//         return navigator.userAgent.match(/Android/i);
-//     },
-//     BlackBerry: function() {
-//         return navigator.userAgent.match(/BlackBerry/i);
-//     },
-//     iOS: function() {
-//         return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-//     },
-//     Opera: function() {
-//         return navigator.userAgent.match(/Opera Mini/i);
-//     },
-//     Windows: function() {
-//         return navigator.userAgent.match(/IEMobile/i);
-//     },
-//     Mac: function() {
-//         return navigator.userAgent.match(/Mac/i);
-//     },
-//     any: function() {
-//         return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-//     }
-// };
-
-
-
-// // js menu navbar
-
-
-// jQuery(function($) {
-//     function adjustNav() {
-//         var winWidth = $(window).width(),
-//             dropdown = $('.dropdown'),
-//             dropdownMenu = $('.dropdown-menu');
-
-//         if (winWidth >= 768) {
-//             dropdown.on('mouseenter', function() {
-//                 $(this).addClass('show')
-//                     .children(dropdownMenu).addClass('show');
-//             });
-
-//             dropdown.on('mouseleave', function() {
-//                 $(this).removeClass('show')
-//                     .children(dropdownMenu).removeClass('show');
-//             });
-//         } else {
-//             dropdown.off('mouseenter mouseleave');
-//         }
-//     }
-
-//     $(window).on('resize', adjustNav);
-
-//     adjustNav();
-
-//     if(isMobile.any()) {
-//         $('.owl-nav button span').css("padding-top", "8px");
-
-//         orig = $.fn.css;
-//         $.fn.css = function() {
-//             var result = orig.apply(this, arguments);
-//             $(this).trigger('stylechanged');
-//             return result;
-//         }
-
-//         //set active tab
-//         $('.owllistProjectHeader .owl-stage').on('stylechanged', function () {
-//             dragTab('.owllistProjectHeader', '#nav-tabContent')
-//         });
-
-//         $('.owlLeaderShip .owl-stage').on('stylechanged', function () {
-//             dragTab('.owlLeaderShip', '#pills-tabContent')
-//         });
-
-//         function dragTab(owlName, tabName) {
-//             setTimeout(function() {
-//                 var winWidth = $(window).width();
-
-//                 if (winWidth < 768) {
-//                     let ele = owlName + ' a';
-//                     $(ele).removeClass('active');
-//                     //
-//                     $(owlName + ' .owl-item').each(function () {
-//                         if ($(this).hasClass('active')) {
-//                             let a = $(this).find('a');
-//                             a.addClass('active');
-
-//                             let href = a.attr('href');
-//                             $(tabName + ' .tab-pane').removeClass('active');
-//                             $(tabName).find(href).addClass('show');
-//                             $(tabName).find(href).addClass('active');
-//                         }
-//                     });
-
-//                 }
-//             }, 500);
-//         }
-
-//         $('body').find('.col').css("flex-basis", "inherit");
-
-//     }
-
-//     if(isMobile.Mac()) {
-//         $('.owl-nav span').css("padding-top", "9px");
-//         $('.imgBanner img').css("width", "100%");
-//         $('.textContent-banner .btn-contact, .numberHome .contact-number').addClass('mac-btn-contact');
-//     }
-// });
+window.onload = function() {
+    window.scrollTo(0, 0);
+};
 
 var elements = document.querySelectorAll('.ant-collapse-item');
 elements.forEach(function(element) {
     element.addEventListener('click', function() {
-        elements.forEach(function(el) {
-            el.classList.remove('active');
-            let eleChildOld =  el.children[1]
-            eleChildOld.classList.remove('active')
-            let elePathOld = el.querySelectorAll('path')
-            elePathOld[0].style.display = 'block';
-        });
-        let eleChild =  element.children[1]
+        // elements.forEach(function(el) {
+        //     el.classList.remove('active');
+        //     let eleChildOld =  el.children[1]
+        //     eleChildOld.classList.remove('active')
+        //     let elePathOld = el.querySelectorAll('path')
+        //     elePathOld[0].style.display = 'block';
+        // });
+        // let eleChild =  element.children[1]
         if(this.classList.contains('active')) {
             this.classList.remove('active');
             eleChild.classList.remove('active');
@@ -277,31 +54,131 @@ function startanim() {
 }
 
 
-tsParticles.load("tsParticles", 
+let btnHeader  = document.querySelector('.header-btnMobile')
+let menuMbl  = document.querySelector('.menu_mbl')
+let btnHeaderClose = document.querySelector('.menu_mbl-action')
+let headerItems = document.querySelectorAll('.menu_mbl-item')
+    btnHeader.addEventListener('click', function(){
+        menuMbl.classList.add('active')
+    })
+    btnHeaderClose.addEventListener('click', function(){
+        console.log(btnHeaderClose)
+        menuMbl.classList.remove('active')
+    })
+    headerItems.forEach(function(item){
+        item.addEventListener('click',function () {
+            menuMbl.classList.remove('active')
+        })
+    })
+
+
+tsParticles.load("tsparticles", 
     {
-        "fps_limit":100,
+        "fps_limit":144,
         "interactivity":{"detectsOn":"canvas",
-        "events":{"onClick":{"enable":true,"mode":"push"},
-        "onHover":{"enable":true,"mode":"repulse"},"resize":true},
+        "events":{"onClick":{"enable":false,"mode":"push"},
+        "onHover":{"enable":false,"mode":"repulse"},"resize":true},
         "modes":{"push":{"particles_nb":4},
         "repulse":{"distance":300,"duration":4}}},
-        "particles":{"color":{"value":"#ffffff"},
-        "links":{"color":"#ffffff","distance":150,"enable":true,"opacity":0.4,"width":3},
+        "particles":{"color":{"value":"#f47a20"},
+        "links":{"color":"#ff8000","distance":150,"enable":true,"opacity":0.5,"width":1},
         "move":{
             "bounce":false,
             "direction":"none",
             "enable":true,
             "outMode":"out",
-            "random":false,
-            "speed":2,
+            "random":true,
+            "speed": 1,
             "straight":false},
-        "number":{"density":{"enable":true,"area":800},"value":80},
+        "number":{"density":{"enable":true,"area":800},"value":70},
         "opacity":{"value":0.5},
         "shape":{"type":"circle"},
         "size":{"random":true,"value":5}},
         "detectRetina":true}
 )
 
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzbRCcykUPNs7A3g34U4AcE7a3xm7DvJaMveFmZPN75TKUa_tiHPnYEHtSMd4Th4rCf/exec'
+
+const form = document.forms['form_contact']
+var fullName = document.querySelector('[name="fullName"]');
+var phone = document.querySelector('[name="Phone"]');
+var email = document.querySelector('[name="Email"]');
+var message = document.querySelector('[name="Message"]');
+
+var nameValid = document.querySelector('#nameValid')
+var phoneValid = document.querySelector('#phoneValid')
+var emailValid = document.querySelector('#emailValid')
+var messValid = document.querySelector('#messValid')
+
+
+
+'<---- Validation Form Contact --->'
+fullName.addEventListener("focusout", function (){
+    if(fullName.value == '' || fullName.value.lenght < 4) {
+        fullName.classList.add('active')
+        nameValid.classList.add('active')
+    }
+    else if(fullName.value !== '') {
+        console.log(fullName.value)
+        fullName.classList.remove('active')
+        nameValid.classList.remove('active')
+    }
+});
+phone.addEventListener("focusout", function (){
+    if(phone.value == '' || phone.value.lenght < 8) {
+        phone.classList.add('active')
+        phoneValid.classList.add('active')
+    } else if(phone.value !== '') {
+        phone.classList.remove('active')
+        phoneValid.classList.remove('active')
+    }
+});
+email.addEventListener("focusout", function (){
+    if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) || email.value == '') {
+        email.classList.add('active')
+        emailValid.classList.add('active')
+    }else if(email.value !== '') {
+        email.classList.remove('active')
+        emailValid.classList.remove('active')
+    }
+});
+message.addEventListener("focusout", function (){
+    if(message.value == "") {
+        message.classList.add('active')
+        messValid.classList.add('active')
+    }else if(email.value !== '') {
+        message.classList.remove('active')
+        messValid.classList.remove('active')
+    }
+});
+
+
+        
+    
+
+form.addEventListener('submit', e => {
+ e.preventDefault()
+ var error = 0
+
+    if(fullName.value == '' || phone.value == '' || email.value == '' || message.value == ''){
+        error++
+    }
+    if(error == 0 ){
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => alert("Cảm ơn ! Chúng tôi sẽ liên hệ bạn trong thời gian sớm nhất."))
+        .then(() => { window.location.reload(); })
+        .catch(error => console.error('Error!', error.message))
+    }
+ 
+})
+
+
+
+
+
+
+  
 
 
 
